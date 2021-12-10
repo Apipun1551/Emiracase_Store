959f665cc2d0bc81db1f6a3d8716aa291e35b869
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-10 22:32:34 +0700

    Menyesuaikan penyimpanan asset template

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -14,15 +14,15 @@
     <link href="https://cdn.materialdesignicons.com/4.4.95/css/materialdesignicons.min.css" rel="stylesheet" />
 
     <!-- PLUGINS CSS STYLE -->
-    <link href="{{ URL::asset('admin/assets/plugins/simplebar/simplebar.css}}" rel="stylesheet" />
-    <link href="assets/plugins/nprogress/nprogress.css" rel="stylesheet" />
+    <link href="{{ URL::asset('admin/assets/plugins/simplebar/simplebar.css')}}" rel="stylesheet" />
+    <link href="{{ URL::asset('admin/assets/plugins/nprogress/nprogress.css')}}" rel="stylesheet" />
 
     <!-- No Extra plugin used -->
-    <link href='assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css' rel='stylesheet'>
-    <link href='assets/plugins/daterangepicker/daterangepicker.css' rel='stylesheet'>
+    <link href='{{ URL::asset('admin/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css')}}' rel='stylesheet'>
+    <link href='{{ URL::asset('admin/assets/plugins/daterangepicker/daterangepicker.css')}}' rel='stylesheet'>
 
 
-    <link href='assets/plugins/toastr/toastr.min.css' rel='stylesheet'>
+    <link href='{{ URL::asset('admin/assets/plugins/toastr/toastr.min.css')}}' rel='stylesheet'>
 
 
 
@@ -31,10 +31,10 @@
 
 
     <!-- SLEEK CSS -->
-    <link id="sleek-css" rel="stylesheet" href="assets/css/sleek.css" />
+    <link id="sleek-css" rel="stylesheet" href="{{ URL::asset('admin/assets/css/sleek.css')}}" />
 
     <!-- FAVICON -->
-    <link href="assets/img/favicon.png" rel="shortcut icon" />
+    <link href="{{ URL::asset('admin/assets/img/favicon.png')}}" rel="shortcut icon" />
 
     <!--
       HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
@@ -44,7 +44,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="assets/plugins/nprogress/nprogress.js"></script>
+    <script src="{{ URL::asset('admin/assets/plugins/nprogress/nprogress.js')}}"></script>
   </head>
 
   <body class="header-fixed sidebar-fixed sidebar-dark header-light" id="body">
@@ -2055,23 +2055,23 @@
     </script> -->
 
     <!-- Javascript -->
-    <script src="assets/plugins/jquery/jquery.min.js"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/plugins/simplebar/simplebar.min.js"></script>
+    <script src="{{ URL::asset('admin/assets/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{ URL::asset('admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ URL::asset('admin/assets/plugins/simplebar/simplebar.min.js')}}"></script>
 
-    <script src='assets/plugins/charts/Chart.min.js'></script>
-    <script src='assets/js/chart.js'></script>
-
-
+    <script src='{{ URL::asset('admin/assets/plugins/charts/Chart.min.js')}}'></script>
+    <script src='{{ URL::asset('admin/assets/js/chart.js')}}'></script>
 
 
-    <script src='assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js'></script>
-    <script src='assets/plugins/jvectormap/jquery-jvectormap-world-mill.js'></script>
-    <script src='assets/js/vector-map.js'></script>
 
-    <script src='assets/plugins/daterangepicker/moment.min.js'></script>
-    <script src='assets/plugins/daterangepicker/daterangepicker.js'></script>
-    <script src='assets/js/date-range.js'></script>
+
+    <script src='{{ URL::asset('admin/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js')}}'></script>
+    <script src='{{ URL::asset('admin/assets/plugins/jvectormap/jquery-jvectormap-world-mill.js')}}'></script>
+    <script src='{{ URL::asset('admin/assets/js/vector-map.js')}}'></script>
+
+    <script src='{{ URL::asset('admin/assets/plugins/daterangepicker/moment.min.js')}}'></script>
+    <script src='{{ URL::asset('admin/assets/plugins/daterangepicker/daterangepicker.js')}}'></script>
+    <script src='{{ URL::asset('admin/assets/js/date-range.js')}}'></script>
 
 
 
