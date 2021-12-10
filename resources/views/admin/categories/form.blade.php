81cdf9b3e89ae0229fd1495d0d283f6ed31cdd38
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-11 01:40:05 +0700

    Membuat form

---
+++ resources/views/admin/categories/form.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.layout')
+
+@section('content')
+<!-- kondisi untuk title -->
+@php
+    $formTitle=!empty($category) ? 'Update' : 'New' //jika objek categori tidak kosong maka update dan sebaliknya
+@endphp
+<!-- Form -->
+<div class="content">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card card-default">
+                <div class="card-header card-header-border-bottom">
+                    <h2>{{$formTitle}} Category</h2>
+                </div>
+                <div class="card-body">
+                    @if (!empty($category)) <!-- tidak memiliki kategori -->
+                        {!! Form::model($category,['url' => ['admin/categories',$category->id],'method'=>'PUT']) !!}
+                        {!! Form::hidden('id') !!}
+                    @else
+                        <!-- Jika kosong masuk ke view -->
+                        {!! Form::open(['url' => 'admin/categories']) !!}
+                    @endif
+                        <div class="form-group">
+                            {!! Form::label('name', 'Name') !!}
+                            {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'category name']) !!}
+                        </div>
+                        <div class="form-footer pt-5 borter-top">
+                            <button type="submit" class="btn btn-primary btn-default"> Simpan </button>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
