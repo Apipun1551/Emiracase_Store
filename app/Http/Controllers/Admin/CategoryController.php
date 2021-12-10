07a2da3a985bc5bb7223a4a9f0c63a7b532f0f16
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-10 23:08:40 +0700

    Menghubungkan index ke view

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,7 +15,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        //menampilkan view
+        $this->data['categories']=Category::orderBy('name','ASC')->paginate(10);
+        return view('admin.categories.index',$this->data);
     }
 
     /**
