c797886cac4651ece746340c120eb041cfb926ec
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-10 22:16:09 +0700

    Mencoba tampilan konsep layouting

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+    </head>
+
+    <body>
+    <h2>Header</h2>
+    <hr>
+        @yield('content')
+    <hr>
+    <h2>Footer</h2>
+    </body>
+</html>
