c44ecca69771eccd0d2f1ae0cb8f86c75c26adb7
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-11 00:25:06 +0700

    Membuat table category

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layout')
+
+@section('content')
+    <div class="content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card card-default">
+                    <div class="card-header card-header-border-bottom">
+                        <h2>Categories</h2>
+                    </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-stripped">
+                        <thead>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Slug</th>
+                            <th>Parent</th>
+                            <th>Action</th>
+                        </thead>
+                        <tbody>
+                            @forelse ($categories as $category)
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td>{{$category->slug}}</td>
+                                    <td>{{$category->parent_id}}</td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5">No record found</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
