e92ae8d77ebe7b34bb8458b6bcbf4f823a703936
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-10 22:53:05 +0700

    Memisahkan bagian footer

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -90,19 +90,7 @@
 
 
     <!-- Footer -->
-    <footer class="footer mt-auto">
-      <div class="copyright bg-white">
-        <p>
-          &copy; <span id="copy-year"></span> Copyright Sleek Dashboard Bootstrap Template by <a class="text-primary"
-            href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
-        </p>
-      </div>
-      <script>
-        var d = new Date();
-        var year = d.getFullYear();
-        document.getElementById("copy-year").innerHTML = year;
-      </script>
-    </footer>
+    @include('admin.partials.footer')
 
     </div> <!-- End Page Wrapper -->
   </div> <!-- End Wrapper -->
