3f4a4a7f28f3607af7c9bdbdc1418aa38ed1415c
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-14 20:44:58 +0700

    Menambahkan auth laravel dan menginstal node.js

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,11 @@ Route::group(
         Route::resource('categories','CategoryController');
     }
 );
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
