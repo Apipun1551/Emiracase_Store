2234b59834943baa16f68b0bcf2a7264aacbbf76
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-15 23:11:14 +0700

    Menambahkan Menu di form

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -8,6 +8,19 @@
 
 <div class="content">
     <div class="row">
+        <div class="col-lg-4">
+            <div class="card card-default">
+                <div class="card-header card-header-border-bottom">
+                    <h2>Product Menus</h2>
+            </div>
+            <div class="card-body">
+                <nav class="nav flex-column">
+                    <a class="nav-link" href="{{url('admin/products/'.$productID.'/edit')}}">Product Detail</a>
+                    <a class="nav-link" href="{{url('admin/products/'.$productID.'/images')}}">Product Images</a>
+                </nav>
+            </div>
+            </div>
+        </div>
         <div class="col-lg-8">
             <div class="card card-default">
                 <div class="card-header card-header-border-bottom">
