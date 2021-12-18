f63353e0fe158fa0310cc671567ca66a69e98c3c
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-18 21:23:12 +0700

    Menambahkan field fillable dan relasi ke product

--- app/Models/ProductAttributeValue.php
+++ app/Models/ProductAttributeValue.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductAttributeValue extends Model
 {
-    //
+    //field fillable
+    protected $fillable= [
+        'product_id',
+        'attribute_id',
+        'text_value',
+        'boolean_value',
+        'integer_value',
+        'float_value',
+        'datetime_value',
+        'date_value',
+        'json_value',
+    ];
+
+    //relasi dengan model product
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
 }
