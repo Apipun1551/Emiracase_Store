0662e2391cb4bf8ebb210d5473fef3282858af5e
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-18 21:09:13 +0700

    deklarasi field dan relasi model

--- app/Models/ProductInventory.php
+++ app/Models/ProductInventory.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductInventory extends Model
 {
-    //
+    //deklarasi field fillable
+    protected $fillable= [
+        'product_id',
+        'qty',
+    ];
+
+    //relasi dengan model product
+    public function product()
+    {
+        //
+        return $this->belongsTo('App\Models\Product');
+    }
 }
