7c7cf83d3d0c03a1af3fe30a118621e4a8fa44c4
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-19 14:21:06 +0700

    Memperbaharui view product

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -10,23 +10,25 @@
                     </div>
                     <div class="card-body">
                         @include('admin.partials.flash')
-                        <table class="table table-bordered table-striped">
+                        <table class="table table-bordered table-stripped">
                             <thead>
                                 <th>#</th>
                                 <th>SKU</th>
+                                <th>Type</th>
                                 <th>Name</th>
                                 <th>Price</th>
                                 <th>Status</th>
-                                <th>Action</th>
+                                <th style="width:15%">Action</th>
                             </thead>
                             <tbody>
                                 @forelse ($products as $product)
                                     <tr>
                                         <td>{{ $product->id }}</td>
                                         <td>{{ $product->sku }}</td>
+                                        <td>{{ $product->type }}</td>
                                         <td>{{ $product->name }}</td>
-                                        <td>{{ $product->price }}</td>
-                                        <td>{{ $product->status}}</td>
+                                        <td>{{ number_format($product->price) }}</td>
+                                        <td>{{ $product->status_label() }}</td>
                                         <td>
                                             <a href="{{ url('admin/products/'. $product->id .'/edit') }}" class="btn btn-warning btn-sm">edit</a>
 
@@ -34,7 +36,6 @@
                                             {!! Form::hidden('_method', 'DELETE') !!}
                                             {!! Form::submit('remove', ['class' => 'btn btn-danger btn-sm']) !!}
                                             {!! Form::close() !!}
-
                                         </td>
                                     </tr>
                                 @empty
