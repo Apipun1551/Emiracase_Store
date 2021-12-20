a6238faac100f67b9c2dbf28402389531ecd1ca5
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-20 11:51:35 +0700

    Menambahkan fungsi Untuk role &permission

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,7 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends Model
+class Permission extends \Spatie\Permission\Models\Permission
 {
-    //
+    //Template dari tutorial di website https://qcode.in/easy-roles-and-permissions-in-laravel-5-4/
+    public static function defaultPermissions()
+    {
+        return [
+            'view_users',
+            'add_users',
+            'edit_users',
+            'delete_users',
+
+            'view_roles',
+            'add_roles',
+            'edit_roles',
+            'delete_roles',
+
+            'view_products',
+            'add_products',
+            'edit_products',
+            'delete_products',
+
+            'view_orders',
+            'add_orders',
+            'edit_orders',
+            'delete_orders',
+        ];
+    }
 }
