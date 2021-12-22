77ea9608ad872f6a06c9afc89db77be7462a2408
Afifa Nurhidayah <poudragneell@gmail.com>
2021-12-22 22:44:39 +0700

    Membatasi selain admin tidak bisa CUD

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -26,11 +26,15 @@
                                     <td>{{$category->slug}}</td>
                                     <td>{{$category->parent? $category->parent->name : ""}}</td>
                                     <td>
-                                        <a href="{{url('admin/categories/'.$category->id.'/edit')}}" class="btn btn-warning btn-sm">edit</a>
-                                        {!! Form::open(['url'=>'admin/categories/'.$category->id,'class'=>'delete','style'=> 'display:inline-block']) !!}
-                                        {!! Form::hidden('_method', 'DELETE') !!}
-                                        {!! Form::submit('remove', ['class'=>'btn btn-danger btn-sm']) !!}
-                                        {!! Form::close() !!}
+                                        @can('edit_categories')
+                                            <a href="{{url('admin/categories/'.$category->id.'/edit')}}" class="btn btn-warning btn-sm">edit</a>
+                                        @endcan
+                                        @can('delete_categories')
+                                            {!! Form::open(['url'=>'admin/categories/'.$category->id,'class'=>'delete','style'=> 'display:inline-block']) !!}
+                                            {!! Form::hidden('_method', 'DELETE') !!}
+                                            {!! Form::submit('remove', ['class'=>'btn btn-danger btn-sm']) !!}
+                                            {!! Form::close() !!}
+                                        @endcan
                                     </td>
                                 </tr>
                             @empty
@@ -43,9 +47,11 @@
                     <!--Memunculkan tombol next pagination-->
                     {{$categories->Links()}}
                 </div>
-                <div class="card-footer text-right">
-                    <a href="{{url('admin/categories/create')}}" class="btn btn-primary">Tambah Data</a>
-                </div>
+                @can('add_categories')
+                    <div class="card-footer text-right">
+                        <a href="{{url('admin/categories/create')}}" class="btn btn-primary">Tambah Data</a>
+                    </div>
+                @endcan
             </div>
         </div>
     </div>
